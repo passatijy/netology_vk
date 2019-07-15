d773cc3667aaf7e5bcae59287c17554f9aa025b3
Marat <lmar@mail.ru>
2019-07-15 16:14:12 +0300

    Решено. Запрашивается ввод токена от ВК, выводится общие пользователей двух доступных мне ID, используется встроенный метод API ВК friends.getMutual

--- userobj.py
+++ userobj.py
@@ -6,44 +6,47 @@ def make_request(method, user_id, access_token):
 	full_url = 'https://api.vk.com/method/' + method + '?user_id=' + user_id + '&v=5.52' + '&access_token=' + access_token + '&fields=city'
 	return requests.get(full_url).json()
 
-def friend_list(friends_dict):
-	fr_list = []
-	if 'response' in friends_dict.keys():
-			for user in friends_dict['response']['items']:
-				fr_list.append(user['first_name'] + '_' + user['last_name'])
-	else :
-		fr_list.append(friends_dict)
-	return fr_list
-
-def get_mutual(src_user, target_user, access_token):
-	full_url = 'https://api.vk.com/method/get_mutual' + '?source_uid=' + src_user + '&target_uid=' + target_user + '&v=5.52' + '&access_token=' + access_token
-
-def friend_list_table(friends_dict):
-	if 'response' in friends_dict.keys():
-			fr_list = []
-			for user in friends_dict['response']['items']:
-				print('Id: ', user['id'],'last_name: ', user['last_name'])
-	else :
-		print('Error get friends:', friends_dict)
-
-
-
-
 class VkUser():
 	def __init__(self, user_id, token):
 		self.token = token
 		self.user_id = user_id
-		self.first_name = make_request('users.get', self.user_id,self.token)['response'][0]['first_name']
-		self.last_name = make_request('users.get', self.user_id,self.token)['response'][0]['last_name']
-		self.friends = make_request('friends.get', self.user_id,self.token)
-	def get_name():
-		pass
-	def get_friends():
-		pass
+		api_response = make_request('users.get', self.user_id,self.token)
+		if 'error' not in api_response.keys():
+			self.first_name = api_response['response'][0]['first_name']
+			self.last_name = api_response['response'][0]['last_name']
+			self.friends = make_request('friends.get', self.user_id, self.token)
+		else:
+			self.first_name = api_response
+			self.last_name = api_response
+			self.friends = api_response
+
+	def friend_list_table(self):
+		if 'response' in self.friends.keys():
+				fr_list = []
+				for user in self.friends['response']['items']:
+					print('Id: ', user['id'],'last_name: ', user['last_name'])
+		else :
+			print('Error get friends:', self.friends)
+
+	def get_mutual(self, target_user):
+		self.target_user = target_user
+		full_url = 'https://api.vk.com/method/friends.getMutual' + '?source_uid=' + self.user_id + '&target_uid=' + self.target_user + '&v=5.52' + '&access_token=' + self.token
+		return requests.get(full_url).json()
+
+	def friend_list(self):
+		fr_list = []
+		if 'response' in self.friends.keys():
+				for user in self.friends['response']['items']:
+					fr_list.append(user['first_name'] + '_' + user['last_name'])
+		else :
+			fr_list.append(self.friends)
+		return fr_list
+
+token = input('Введите токен: ')
 
 
+petrenko = VkUser('552646270',token)
+belousova = VkUser('552646270', token)
 
-print('Korol and sidor mutual: ',get_mutual('547233513', '550885673', token))
-print('Smir and pon mutual: ', get_mutual('550886021', '551160869', token))
-print('petren and bel mutual: ', get_mutual('552610377', '552646270', token))
-print('pug and cherne mutual: ', get_mutual('552748481', '552762252', token))
+
+print('Petrenko and bel mutual:', petrenko.get_mutual(bel.user_id))
